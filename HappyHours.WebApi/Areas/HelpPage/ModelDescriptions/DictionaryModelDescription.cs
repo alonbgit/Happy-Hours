b16cf3d9f5837be74efb3a603d95804d82cc1ff3
Alon <--global 3alonb@gmail.com>
2017-08-22 19:56:23 +0300

    commit

---
+++ HappyHours.WebApi/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace HappyHours.WebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ HappyHours.WebApi/Attributes/ModelStateAttribute.cs
@@ -0,0 +1,41 @@
+﻿using HappyHours.WebApi.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Filters;
+using System.Web.Http.Controllers;
+using HappyHours.Models.Common;
+using System.Net.Http;
+
+namespace HappyHours.WebApi.Attributes
+{
+    public class ModelStateAttribute : ActionFilterAttribute, IOrderedFilter
+    {
+        public ModelStateAttribute(int order)
+        {
+            this.Order = order;
+        }
+
+        public override void OnActionExecuting(HttpActionContext actionContext)
+        {
+            if (!actionContext.ModelState.IsValid)
+            {
+                var errorResponse = new BaseResponse()
+                {
+                    ErrorCode = ErrorCode.BadRequest
+                };
+
+                var response = actionContext.Request.CreateResponse<BaseResponse>(errorResponse);
+                actionContext.Response = response;
+            }
+
+            base.OnActionExecuting(actionContext);
+        }
+
+        public int Order
+        {
+            get; set;
+        }
+    }
+}
